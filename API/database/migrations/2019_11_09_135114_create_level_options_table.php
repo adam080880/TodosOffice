9931bb7726d5d4e95762afc0b58b9a025deedb84
muhamadadam20 <muhamadadam20@gmail.com>
2019-11-09 23:58:59 +0700

    Init first tables

--- API/database/migrations/2019_11_09_135114_create_level_options_table.php
+++ API/database/migrations/2019_11_09_135114_create_level_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateLevelOptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('level_options', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id');        
-            $table->rememberToken();
+            $table->string('title');
+            $table->unsignedBigInteger('level_id');
+            $table->integer('minimal_points');
+            $table->text('image_url');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('level_options');
     }
 }
