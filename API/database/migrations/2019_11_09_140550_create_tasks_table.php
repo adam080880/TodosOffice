acdc1166270b4bddcc6800a30b940dc8cdce303b
muhamadadam20 <muhamadadam20@gmail.com>
2019-11-10 17:10:02 +0700

    add description

--- API/database/migrations/2019_11_09_140550_create_tasks_table.php
+++ API/database/migrations/2019_11_09_140550_create_tasks_table.php
@@ -16,6 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('task');
+            $table->text('description');
             $table->integer('points');        
             $table->timestamps();
         });
