759b2e7a9bd13ba5e5ccf7b9de0672ab49c1e8fe
muhamadadam20 <muhamadadam20@gmail.com>
2019-11-11 09:35:15 +0700

    Add controller taskfor

---
+++ API/app/Http/Controllers/TaskForController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\TaskFor;
+
+class TaskForController extends Controller
+{
+    public function get()
+    {
+        
+    }
+}
