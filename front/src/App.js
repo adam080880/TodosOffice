a1d129c25973b87de657f0c3045bcbadc11ce616
muhamadadam20 <muhamadadam20@gmail.com>
2019-11-20 16:29:51 +0700

    initial init

---
+++ front/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+function App()
+{
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
